3aaf598ac81cdcee47c8dad4cf3378da078819d8
AnaLGiN83 <zyk83@mail.ru>
2022-01-08 16:33:42 +0300

    Salt forage skill change to 3

--- media/lua/shared/Foraging/ANL_RenewableFoodForageItems.lua
+++ media/lua/shared/Foraging/ANL_RenewableFoodForageItems.lua
@@ -7,7 +7,7 @@ local SmallSaltRock = {
 	minCount=1,
 	maxCount=3,
 	xp=15,
-	skill=2,
+	skill=3,
 	categories = { "Stones" },
 	zones={ Forest=25, DeepForest=25, FarmLand=20, Farm=15, Vegitation=30 },
 	spawnFuncs = { doWildFoodSpawn }
